e53aef9531cb7e41a850cb1cd6ad4becfa9181e7
Jahongir611 <khurramovjahongir611@gmail.com>
2023-03-13 18:15:49 +0500

    #

--- js/script.js
+++ js/script.js
@@ -23,3 +23,32 @@ button_menu.addEventListener('click', function () {
     center_line.classList.toggle('delete-center')
     bottom_line.classList.toggle('rotate-bottom')
 })
+
+let yakar1 = doc.querySelector('.yakar1')
+let yakar2 = doc.querySelector('.yakar2')
+let yakar3 = doc.querySelector('.yakar3')
+let yakar4 = doc.querySelector('.yakar4')
+
+let href1 = doc.querySelector('#kotegoriya')
+let href2 = doc.querySelector('#podpiska')
+let href3 = doc.querySelector('#contacts')
+let href4 = doc.querySelector('#onas')
+
+
+// Это можно было сделать через css
+yakar1.addEventListener('click',(event) => {
+    event.preventDefault()
+    window.scrollTo({top: href1.offsetTop - 150, behavior: 'smooth'})
+})
+yakar2.addEventListener('click',(event) => {
+    event.preventDefault()
+    window.scrollTo({top: href2.offsetTop - 150, behavior: 'smooth'})   
+})
+yakar3.addEventListener('click',(event) => {
+    event.preventDefault()
+    window.scrollTo({top: href3.offsetTop - 150, behavior: 'smooth'})   
+})
+yakar4.addEventListener('click',(event) => {
+    event.preventDefault()
+    window.scrollTo({top: href4.offsetTop - 150, behavior: 'smooth'})   
+})
